3110426e4524414d3892c49b1d810d4a0fdee9d1
Michael Rich <mtrich@satx.rr.com>
2018-09-14 05:30:06 -0500

    checkpoint03

---
+++ checkpoint03/js/script.js
@@ -0,0 +1,32 @@
+var data = {
+  totalMoney:0,
+  income: 0
+};
+
+setInterval(addIncome,1000);
+
+function addIncome() {
+  data.totalMoney += data.income;
+  updateReport();
+}
+
+function updateReport() {
+  $("#currentTotal").text('$'+Math.floor(data.totalMoney));
+  $("#income").text((data.income).toFixed(2));
+}
+
+$("#dollar").click(function (){
+  data.totalMoney ++;
+  updateReport();
+})
+
+$(".button").click(function (){
+  var addVal = $(this).data( "cost" );
+  if ($(this).data( "cost" ) <= data.totalMoney ) {
+    data.totalMoney -=  parseFloat($(this).data( "cost" ).toPrecision(2));
+    data.income += parseFloat($(this).data( "val" ));
+    $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15));
+    $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
+  }
+  updateReport();
+})
